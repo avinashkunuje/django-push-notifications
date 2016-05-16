de13f1debf1585ab8a2791ad0453f3d2f38d3d50
avinashkunuje <itsavi.ks@gmail.com>
2016-05-16 16:40:48 +0530

    initial commit,New repo created

---
+++ demo/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from swampdragon.models import SelfPublishModel
+from .serializers import NotificationSerializer
+
+
+class Notification(SelfPublishModel, models.Model):
+    serializer_class = NotificationSerializer
+    message = models.TextField()
+
+
+
---
+++ demo/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+from django.views.generic import ListView
+from .models import Notification
+
+
+class Notifications(ListView):
+    model = Notification
+    template_name = 'home.html'
+
+    def get_queryset(self):
+        return self.model.objects.order_by('-pk')[:5]
